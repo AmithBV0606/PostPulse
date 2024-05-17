16fcc8286f2a9dfd48d8229c8b7703c661a9f2f1
AmithBV0606 <amithrao0606@gmail.com>
2024-05-17 20:20:49 +0530

    configured store and reducers, created Header and Footer components

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,38 @@
-function App() {
+import { useState, useEffect } from "react";
+import { useDispatch } from "react-redux";
+import authService from "./appwrite/auth";
+import { login, logout } from "./features/authSlice";
+import { Header, Footer } from "./components";
+import { Outlet } from "react-router-dom";
 
-  return (
-    <>
-      <h1>PostPulse - A Bloggong App</h1>
-    </>
-  )
+function App() {
+  const [loading, setLoading] = useState(true);
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    authService
+      .getCurrentUser()
+      .then((userData) => {
+        if (userData) {
+          dispatch(login({ userData }));
+        } else {
+          dispatch(logout());
+        }
+      })
+      .finally(() => setLoading(false));
+  }, []);
+
+  return !loading ? (
+    <div className="min-h-screen flex flex-wrap content-between bg-gray-500">
+      <div className="w-full block">
+        <Header />
+        {/* <main>
+            <Outlet />
+          </main> */}
+        <Footer />
+      </div>
+    </div>
+  ) : null
 }
 
-export default App
+export default App;
